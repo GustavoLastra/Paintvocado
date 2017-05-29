03b542d31c09dbd482ebd4bb827b6db2199f7ffb
Gustavo Lastra <gustavo.lastra@bee.io>
2017-05-29 03:08:57 +0200

    choose color

--- js/Paint.js
+++ js/Paint.js
@@ -7,7 +7,7 @@ myPaintArea.create(); //call to myPaintArea to create the canvas
 var pencil = {
   draw : function(xPos, yPos) {
    myPaintArea.context.beginPath();
-   myPaintArea.context.fillStyle = "blue";
+   myPaintArea.context.fillStyle = $('input[type=color]').val();
    myPaintArea.context.arc(xPos - $('#myCanvas').offset().left, yPos - $('#myCanvas').offset().top, $('input[type=range]').val(), 0, 2 * Math.PI);
    myPaintArea.context.fill();
    myPaintArea.context.closePath();
@@ -15,7 +15,6 @@ var pencil = {
  }
 }
 //pencil.draw();
-
 function reset() {
   myPaintArea.clear();
 }
